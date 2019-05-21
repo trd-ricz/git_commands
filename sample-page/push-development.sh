0fda6234c098afdb4c342d1fcd2f06aa8f31bbef
trd-ricz <trd.ricz@gmail.com>
2019-05-21 16:36:35 +0800

    test

--- sample-page/push-development.sh
+++ sample-page/push-development.sh
@@ -8,7 +8,7 @@
 SSH_USER=root@108.160.134.120
 
 TAR_FILE=sample.tar.gz
-SSH_PARAMS="${SSH_USER}"
+SSH_PARAMS="${KEY_FILE} ${SSH_USER}"
 DATE=`date +%Y%m%d_%H-%M-%S`
 DEPLOY_DIR=/usr/share/nginx/html
 
@@ -16,8 +16,7 @@ DEPLOY_DIR=/usr/share/nginx/html
 # ssh upload
 ####################################
 # upload
-#/usr/bin/scp ${TAR_FILE} ${SSH_USER}:${DEPLOY_DIR}
-
+#/usr/bin/scp -i ${KEY_FILE} ${TAR_FILE} ${SSH_USER}:${DEPLOY_DIR}
 
 #################
 # comands prepare
@@ -32,4 +31,4 @@ echo "yehey!"
 #################
 # exec commands
 #################
-ssh ${SSH_PARAMS} -o "StrictHostKeyChecking no" ${cmds[*]}
+ssh -i ${SSH_PARAMS} -o "StrictHostKeyChecking no" ${cmds[*]}
