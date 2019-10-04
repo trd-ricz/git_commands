34179d3940e241132428c08873b46c359bc4f905
trd-ricz <trd.ricz@gmail.com>
2019-10-04 16:46:11 +0800

    prepare auto deploy

--- ci/deploy-development.sh
+++ ci/deploy-development.sh
@@ -6,6 +6,20 @@ CIRCLE_BRANCH=development
 set -xeu
 
 cd sample-page
-tar -zcvf sample.tar.gz *
 
-bash push-development.sh 
+##cp docker-compose.yml
+docker network create shared
+
+# docker-compose up -d 
+docker-compose up -d
+
+# install npm
+docker-compose exec workspace npm install
+
+# build project
+docker-compose exec workspace npm run build
+
+#zip build file
+tar -zcvf sample.tar.gz build/*
+
+bash push-development.sh
