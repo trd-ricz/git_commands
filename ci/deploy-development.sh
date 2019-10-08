8493fe380ecc235062948f26e5d62b8d0889a3d4
trd-ricz <trd.ricz@gmail.com>
2019-10-08 15:18:32 +0800

    fix test200

--- ci/deploy-development.sh
+++ ci/deploy-development.sh
@@ -16,11 +16,12 @@ set -xeu
 # install npm
 cd sample-page
 
+curl -o- -L https://yarnpkg.com/install.sh | bash
+
 yarn install
 
-
 # build project
-#npm run build
+yarn run build
 
 #zip build file
 tar -zcvf sample.tar.gz build/*
