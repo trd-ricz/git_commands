fcb692eeeba165f10a0f6c7ee6a30718dd915c72
trd-ricz <trd.ricz@gmail.com>
2020-12-11 15:12:14 +0800

    qweqqqe

--- ci/test.sh
+++ ci/test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CI_TEST=$1
+CI_TEST=$TEST
 
-echo $1
-echo ${CI_TEST}
-
-sed -i'.bak' "s/${TEST}/${CI_TEST}/g" ./sample-page/.env;
+sed -i'.bak' 's/${TEST}/${CI_TEST}/g' ./sample-page/.env;
